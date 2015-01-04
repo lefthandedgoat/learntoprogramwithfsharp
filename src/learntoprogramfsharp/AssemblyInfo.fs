03161f90caa37b744770bf1575001b46f3410333
Chris Holt <lefthandedgoat@gmail.com>
2015-01-03 18:09:22 -0600

    I have no clue what I am doing with project template but here goes nothing

---
+++ src/learntoprogramfsharp/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("learntoprogramfsharp")>]
+[<assembly: AssemblyProductAttribute("learntoprogramfsharp")>]
+[<assembly: AssemblyDescriptionAttribute("a book to teach people how to program")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
